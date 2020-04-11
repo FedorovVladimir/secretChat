6bb3dd628a82bf95261e0a651b52bc934b5f33ad
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-04-11 20:50:23 +0700

    Initial commit

---
+++ src/client/Client.java
@@ -0,0 +1,83 @@
+package client;
+
+import network.TCPConnection;
+import network.TCPConnectionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class Client extends JFrame implements ActionListener, TCPConnectionListener {
+
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("vladimir");
+    private final JTextField fieldInput = new JTextField();
+    private TCPConnection connection;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Client::new);
+    }
+
+    public Client() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+        setVisible(true);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMsg("Connection exception: " + e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        String msg = fieldInput.getText();
+        if (msg.equals("")) {
+            return;
+        }
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + msg);
+    }
+
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection ready...");
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMsg(value);
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection close");
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exception: " + e);
+    }
+
+    private synchronized void printMsg(String msg) {
+        SwingUtilities.invokeLater(() -> {
+            log.append(msg + "\n");
+            log.setCaretPosition(log.getDocument().getLength());
+        });
+    }
+}
---
+++ src/server/Server.java
@@ -0,0 +1,62 @@
+package server;
+
+import network.TCPConnection;
+import network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server implements TCPConnectionListener {
+
+    private final List<TCPConnection> connections = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    private Server() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection.toString());
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection.toString());
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        for (TCPConnection connection : connections) {
+            connection.sendString(value);
+        }
+    }
+}
